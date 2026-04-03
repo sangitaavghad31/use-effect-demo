1d83755c9b5dda6083ea0bf86a41c022096ccc7b
sangita avghad <sangitaavghad1987@gmail.com>
2026-04-03 13:47:41 +0530

    Sangita: making use of usecontext to aviod props chaining

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // Write your code at relevant places in the code below:
 
-import React, { useEffect, useState, usseEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,13 +30,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider value={{
+         isLoggedIn:isLoggedIn,
+          onLogout:logoutHandler
+      }}>
+        <MainHeader/>
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
   );
 }
 
